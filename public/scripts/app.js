c312449a81d94b8891b5ec2c031775f3f0a817ae
KamajiiSC <brandonlschaen@gmail.com>
2020-01-25 12:26:06 -0500

    Created app div in index and appRoot variable in app, and then created a render

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,4 +2,6 @@ console.log('App.js is running!');
 
 //JSX - Javascript XML
 let template = <p>This is JSX from app.js!</p>;
+let appRoot = document.getElementById("app");
 
+ReactDOM.render(template, appRoot);
