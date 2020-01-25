7adcbb3d1482870451c7de05720ecf88f58bb54d
KamajiiSC <brandonlschaen@gmail.com>
2020-01-25 12:32:13 -0500

    Used babel to make jsx readable

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,8 @@
 console.log('App.js is running!');
 
 //JSX - Javascript XML
-let template = <p>This is JSX from app.js!</p>;
+//let template = <p>This is JSX from app.js!</p>;
+var template = React.createElement("p", null, "This is JSX from app.js!");
 let appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
