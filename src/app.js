2eff322b9cc5e1b785aaf1f436936c5bc69bfb1a
KamajiiSC <brandonlschaen@gmail.com>
2020-01-28 15:02:02 -0500

    Changed p element to h1

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 console.log('App.js is running!');
 
 //JSX - Javascript XML
-let template = <p>This i JSX from app.js!</p>;
+let template = <h1>This i JSX from app.js!</h1>;
 let appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
