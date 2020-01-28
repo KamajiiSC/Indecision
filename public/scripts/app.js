7c99a0eb99ff2a7b402333d9e08060ce087b63d3
KamajiiSC <brandonlschaen@gmail.com>
2020-01-28 15:10:40 -0500

    Entered babel command in a commment and then fixed a word

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,13 @@
 "use strict";
 
 console.log('App.js is running!');
+//babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 
 //JSX - Javascript XML
 var template = React.createElement(
-  "p",
+  "h1",
   null,
-  "This i JSX from app.js!"
+  "This is JSX from app.js!"
 );
 var appRoot = document.getElementById("app");
 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 console.log('App.js is running!');
+//babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 
 //JSX - Javascript XML
-let template = <h1>This i JSX from app.js!</h1>;
+let template = <h1>This is JSX from app.js!</h1>;
 let appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
