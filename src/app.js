f830d53827c4c636b16cd412a858ef12fb89daa4
KamajiiSC <brandonlschaen@gmail.com>
2020-02-06 18:28:00 -0500

    Added class button to button

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ let count = 0;
 const templateTwo =(
   <div>
     <h1>Count: {count}</h1>
-    <button id="my-id">+1</button>
+    <button id="my-id" className="button">+1</button>
   </div>
 );
 
