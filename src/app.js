5e78bd3a9e0220626ec4dea07433b99f2502c3eb
KamajiiSC <brandonlschaen@gmail.com>
2020-02-21 19:54:50 -0500

    Created renderOptions that creates the template and renders it, and called it in the onFormSubmit function and at the bottom of the script

--- src/app.js
+++ src/app.js
@@ -8,6 +8,26 @@ const app ={
   options: ['one', 'two']
 };
 
+const renderOptions = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{(app.options.length > 0 )? "Here are your options" : "There are no options"}</p>
+      <p>{app.options.length}</p>
+      <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type='text' name='option' />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  ReactDOM.render(template, appRoot);
+};
+
 const onFormSubmit = (e) => {
   e.preventDefault();
 
@@ -17,24 +37,11 @@ const onFormSubmit = (e) => {
   if (option) {
     app.options.push(option);
     e.target.elements.option.value = ' ';
+    renderOptions();
   }
 };
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{(app.options.length > 0 )? "Here are your options" : "There are no options"}</p>
-    <ol>
-    <li>Item one</li>
-    <li>Item two</li>
-    </ol>
-    <form onSubmit={onFormSubmit}>
-      <input type='text' name='option' />
-      <button>Add Option</button>
-    </form>
-  </div>
-);
+
 
 const appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+renderOptions();
