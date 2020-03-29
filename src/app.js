b905909a1b88b98909c77edb137ffc0ddf9c20af
unknown <brandonlschaen@gmail.com>
2020-03-29 17:04:30 -0400

    Created a button in the action component

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ class Header extends React.Component{
 class Action extends React.Component{
   render() {
     return(
-      
+      <div>
+        <button>What should I do</button>
+      </div>
     )
   }
 }
