16fdb70fd839cbcf136f5570e61af03d25b148af
KamajiiSC <brandonlschaen@gmail.com>
2020-04-21 19:50:03 -0400

    Made subtitle only show if there is a value

--- src/app.js
+++ src/app.js
@@ -61,7 +61,8 @@ const Header = (props) => {
   return(
     <div>
       <h1>{props.title}</h1>
-      <h2>{props.subtitle}</h2>
+      {props.subtitle && <h2>{props.subtitle}</h2>
+}
     </div>
   );
 }
