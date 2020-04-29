4f291586ad39044c8af18e496f7cd9cee08c4226
KamajiiSC <brandonlschaen@gmail.com>
2020-04-29 18:32:37 -0400

    Moved state out of constructor of IndecisionApp and then changed the event handlers into arrow functions so I could delete the constructor entirely

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,16 +5,10 @@ import Header from './Header';
 import Options from './Options';
 
 export default class IndecisionApp extends React.Component{
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.state = {
-      options: []
-    }
+  state = {
+    options: []
   }
+
   componentDidMount() {
     try{
       const json = localStorage.getItem('options');
@@ -34,19 +28,19 @@ export default class IndecisionApp extends React.Component{
     }
   }
 
-  handlePick() {
+  handlePick = () => {
     let pick = this.state.options[Math.floor(Math.random() * this.state.options.length)];
     alert(pick);
   }
-  handleDeleteOptions() {
+  handleDeleteOptions = () => {
     this.setState(() => ({options: []}));
   }
-  handleDeleteOption(optionToRemove){
+  handleDeleteOption = (optionToRemove) => {
     this.setState((prevState) => ({
       options: prevState.options.filter((option) => option !== optionToRemove)
     }));
   }
-  handleAddOption(option) {
+  handleAddOption = (option) => {
     if(!option) {
       return 'Enter valid value to add item';
     }else if (this.state.options.indexOf(option) > -1) {
