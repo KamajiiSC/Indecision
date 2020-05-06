0f8ce3f00da579866965e7da71817cba387b6b4f
KamajiiSC <brandonlschaen@gmail.com>
2020-05-06 19:37:09 -0400

    Set .modal background to light-blue

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,6 +7,7 @@ const OptionModal = (props) => (
     contentLabel="Selected Option"
     onRequestClose={props.handleClearSelectedOption}
     closeTimeoutMS={200}
+    className="modal"
   >
     <h3>Selected Option</h3>
     {props.selectedOption && <p>{props.selectedOption}</p>}
